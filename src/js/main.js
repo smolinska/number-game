6d963d01b8853c86176708cf8deb3bbd5899e476
Paulina <smolinska.ps@gmail.com>
2017-03-31 22:13:42 +0200

    difficulty

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 let pickedNumber = Math.floor(Math.random() * 24) + 1;
 console.log(pickedNumber);
 
+const difficulty = {
+    hard: 2,
+    medium: 3,
+    easy: 4,
+};
 
 $('form .btn').click(function () {
-    let yourNumber = $('form input').val();
+    let form = document.getElementById('game-form');
+    let yourNumber = form['your-number'].value;
+    let pickedDifficulty = difficulty[form.difficulty.value];
 
-    if (pickedNumber === yourNumber) {
+
+    let difference = Math.abs(pickedNumber - yourNumber);
+    if (difference <= pickedDifficulty) {
         alert('You win!');
     } else {
         alert('You loose!');
