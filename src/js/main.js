3d03ee098fe06d406b6af26407fe663497747f32
Paulina <smolinska.ps@gmail.com>
2017-03-31 21:31:20 +0200

    random number

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-let s = 'working';
-console.log(`Iam ${s}`);
+let pickedNumber = Math.floor(Math.random() * 24) + 1;
+console.log(pickedNumber);
+
+
+$('form .btn').click(function () {
+    let yourNumber = $('form input').val();
+
+    if (pickedNumber === yourNumber) {
+        alert('You win!');
+    } else {
+        alert('You loose!');
+    }
+});
