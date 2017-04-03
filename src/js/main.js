02009ddaaef6e5454ae415aac7ab098acff10107
Paulina <smolinska.ps@gmail.com>
2017-04-03 20:11:39 +0200

    font awesome

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
-let pickedNumber = Math.floor(Math.random() * 24) + 1;
-console.log(pickedNumber);
-
 const difficulty = {
     hard: 2,
     medium: 3,
     easy: 4,
 };
 
-$('form .btn').click(function () {
+$('form .btn').click(function (event) {
+    let pickedNumber = Math.floor(Math.random() * 24) + 1;
+    console.log(pickedNumber);
+    event.preventDefault();
     let form = document.getElementById('game-form');
     let yourNumber = form['your-number'].value;
     let pickedDifficulty = difficulty[form.difficulty.value];
@@ -15,8 +15,10 @@ $('form .btn').click(function () {
 
     let difference = Math.abs(pickedNumber - yourNumber);
     if (difference <= pickedDifficulty) {
-        alert('You win!');
+        $('#feedback').html(`<i class="fa fa-check" aria-hidden="true" style="font-size:50px"></i><br>You win!<br>Number was: ${pickedNumber}`);
     } else {
-        alert('You loose!');
+        $('#feedback').html(`<i class="fa fa-times" aria-hidden="true" style="font-size:50px"></i><br>You loose!<br>Number was: ${pickedNumber}`);
     }
+
+    form['your-number'].value = '';
 });
